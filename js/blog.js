fbea18239999e93d74a229de60f125080e842b8d
shilinniao <shilinniao@qq.com>
2022-04-14 14:02:54 +0800

    Site updated: 2022-04-14 14:02:53

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,3 @@
-// 设置页脚博主会动的心
-$(document).ready(function (e) {
-    $('.copyright').html('©2022 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 失落森林');
-})
 
 // 移除页脚样式
 var app = document.getElementById('footer');
